570b58f72e005d7393e4d1d4f55bc3754545e76f
Hudson Menezes <hudson.bm23@gmail.com>
2022-10-03 20:19:31 -0300

    conectado ao backend e dotenv instalado

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
-import express from 'express'
+// import express from 'express'
+// import mysql from 'mysql'
+const express = require('express')
+const mysql = require('mysql')
+require('dotenv').config({ path: 'variaveis.env' })
 
 const app = express()
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+})
 
 app.listen(8800, () => {
-  console.log('Conectado ao backend')
+  console.log('Conectado ao backend!!')
 })
