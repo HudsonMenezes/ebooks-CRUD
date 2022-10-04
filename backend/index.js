5b754161b73224ede65f2918b424d1b25de88888
Hudson Menezes <hudson.bm23@gmail.com>
2022-10-03 21:30:02 -0300

    testes

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,12 @@
 const express = require('express')
 const mysql = require('mysql')
 require('dotenv').config({ path: 'variaveis.env' })
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -16,11 +20,22 @@ app.get('/', (req, res) => {
   res.json('Hello this is the dark force')
 })
 
-app.get("/books", (req, res) => {
-  const q = "SELECT * FROM books"
+app.get('/books', (req, res) => {
+  const q = 'SELECT * FROM books'
   db.query(q, (err, data) => {
-    if(err) return res.json(erro)
+    if (err) return res.json(err)
     return res.json(data)
+  })
+})
+
+app.post('/books', (req, res) => {
+  const q = 'INSERT INTO books (`title`, `description`, `cover`) VALUES (?)'
+  const values = ['titulo aqui', 'descricao aqui', 'foto qualquer']
+
+  console.log(values)
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err)
+    return res.json('Book has been created successfully.')
   })
 })
 
